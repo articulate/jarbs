0db7a8aec0dfc507bb37f7ed78a8a3e70191b9ac
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-10-29 15:27:00 -0400

    no need to debug the compiled assets
    just outputs the .zip for debugging

--- lib/jarbs/compiler.rb
+++ lib/jarbs/compiler.rb
@@ -14,23 +14,6 @@ module Jarbs
       @function.each_file do |file, source|
         compiled_info = Babel::Transpiler.transform source
         @function.update(file, compiled_info['code'])
-
-        write_compiled(file, compiled_info['code']) if $debug
-      end
-
-      say_warning "DEBUG: Compiled source output to #{@function.debug_path}" if $debug
-    end
-
-    private
-
-
-    def write_compiled(file, src)
-      debug_file_path = File.join(@function.debug_path, file)
-      root_dir_path = File.dirname(debug_file_path)
-
-      FileUtils.mkdir_p root_dir_path unless Dir.exists? root_dir_path
-      File.open(debug_file_path, 'w') do |debug_out|
-        debug_out.write(src)
       end
     end
   end
