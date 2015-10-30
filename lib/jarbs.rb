1bca40f9f6aa47cd718766a9ed9195397f947b0d
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-10-30 17:01:15 -0400

    drop commands not ready yet

--- lib/jarbs.rb
+++ lib/jarbs.rb
@@ -79,28 +79,6 @@ module Jarbs
         end
       end
 
-      command :logs do |c|
-        c.syntax = 'jarbs logs [options]'
-        c.summary = ''
-        c.description = ''
-        c.example 'description', 'command example'
-        c.option '--some-switch', 'Some switch that does something'
-        c.action do |args, options|
-          # Do something or c.when_called Jarbs::Commands::Logs
-        end
-      end
-
-      command :run do |c|
-        c.syntax = 'jarbs run [options]'
-        c.summary = ''
-        c.description = ''
-        c.example 'description', 'command example'
-        c.option '--some-switch', 'Some switch that does something'
-        c.action do |args, options|
-          # Do something or c.when_called Jarbs::Commands::Run
-        end
-      end
-
       run!
     end
   end
